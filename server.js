a85319e16b5eb3d6fafebde5cddf255847ef5aef
Dalton Wais <georgedaltonwais@gmail.com>
2019-04-25 13:26:33 -0600

    simplify initial function calls

--- server.js
+++ server.js
@@ -11,20 +11,16 @@ global.logMessage = (msg) => {
 
 let populator = require('./server/populate');
 
-let StartApplication = async () => {
-    global.logMessage('connecting to mongodb at ' + config.DB_URL);
-    mongoose.connect(process.env.DB_URL || config.DB_URL, { useNewUrlParser: true });
-    app.listen(process.env.PORT || 5000);
-    global.logMessage('api running on port ' + config.PORT);
-};
-let RunApplication = async() => {
+(async() => {
     let response = await populator();
     if (response === true) {
-        StartApplication().catch(err => global.logMessage(err));
+        (async () => {
+            global.logMessage('connecting to mongodb at ' + config.DB_URL);
+            mongoose.connect(process.env.DB_URL || config.DB_URL, { useNewUrlParser: true });
+            app.listen(process.env.PORT || 5000);
+            global.logMessage('api running on port ' + config.PORT);
+        })().catch(err => global.logMessage(err));
     }
-};
-
-
-RunApplication();
+})();
 
 exports = module.exports = app;
